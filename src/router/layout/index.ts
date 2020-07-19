226231ed8b927bda6ff3f5784267e98e5a6a6ee0
张一 <2326962137@qq.com>
2020-07-19 23:02:33 +0800

    修改 菜单

--- src/router/layout/index.ts
+++ src/router/layout/index.ts
@@ -5,13 +5,82 @@ import db from '../../db';
 const layout = express.Router();
 const data = [];
 
-layout.get('/layout/getLogo', (req: any, res) => {
+layout.get('/layout/logo', (req: any, res) => {
   res.send({
     code: 0,
     msg: 'ok',
     data: {
       url: 'http://www.ndzy01.com:8888/uploads/2020-6-15-9-25-48-32x32.ico',
     },
+  });
+});
+
+layout.get('/layout/menu', (req: any, res) => {
+  res.send({
+    code: 0,
+    msg: 'ok',
+    data: [
+      {
+        id: 1,
+        pId: 0,
+        name: 'ts',
+        url: '/',
+        type: 0,
+        menuIcon: 'icon-yun icon-yun_usericon_szy',
+        children: [
+          {
+            id: 11,
+            pId: 10,
+            name: 'ahooks',
+            url: '/ahooks',
+            menuIcon: 'ahooks',
+            children: [
+              {
+                id: 111,
+                pId: 11,
+                name: 'lifeCycle',
+                url: '/ahooks/lifeCycle',
+                menuIcon: '',
+                children: [],
+              },
+              {
+                id: 112,
+                pId: 11,
+                name: 'useRequest',
+                url: '/ahooks/useRequest',
+                menuIcon: '',
+                children: [],
+              },
+            ],
+          },
+          {
+            id: 12,
+            pId: 10,
+            name: '工作记录',
+            url: '/worRecordk',
+            menuIcon: 'worRecordk',
+            children: [
+              {
+                id: 121,
+                pId: 12,
+                name: '添加记录',
+                url: '/addworkRecord',
+                menuIcon: '',
+                children: [],
+              },
+              {
+                id: 122,
+                pId: 12,
+                name: '展示与查询',
+                url: '/workRecords',
+                menuIcon: '',
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
   });
 });
 
